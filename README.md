da083bc1338917dd099e563d792aadb658671f2d
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-11-26 11:13:02 +0600

    rm all

--- README.md
+++
@@ -1,1 +0,0 @@
-http://gpglobal.b2mwap.com/api/consent/prepare/P1D/P1/01701677479
--- resources/js/app.js
+++
@@ -1,6 +0,0 @@
-require('./bootstrap');
-require('./operator');
-require('./country');
-require('./service');
-require('./campaign');
-require('./traffic');
